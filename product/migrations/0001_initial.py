2c1e5816320b5b69c004558a8affe409450be282
Jaydeep <jaydipgajera2020@gmail.com>
2021-09-19 18:53:01 +0530

    Done

--- product/migrations/0001_initial.py
+++ product/migrations/0001_initial.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
             name='Product',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
+                ('name', models.TextField()),
                 ('description', models.TextField()),
                 ('pic', models.ImageField(upload_to='products')),
                 ('price', models.DecimalField(decimal_places=2, max_digits=8)),
